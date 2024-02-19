acabc4d398a6b2e8c029a7145e4aacf4a2b28fa3
parksg <park0110725@naver.com>
2024-02-19 12:41:08 +0900

    처음 만들어본 자바 코드

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,14 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println("Hello and 상규!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.println("Hi java!");
+        System.out.println("Hola Intellij");
+
+        System.out.println("안녕 중앙정보");
+        System.out.println("ㅎㅎㅎ");
+
+
     }
 }
---
+++ src/Main3.java
@@ -0,0 +1,11 @@
+public class Main3 {
+    // main
+    public static void main(String[] args) {
+
+        // sout
+        System.out.println("하루 물 2리터 마시기");
+        System.out.println("운동30분하기");
+        System.out.println("비타민먹기");
+
+    }
+}
